67293412025acc4779b9eb925d9bce204f4fc06a
max <imloama@gmail.com>
2020-01-03 21:03:30 +0800

    init

---
+++ boot-admin/src/test/java/com/github/imloama/bootadmin/BootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.imloama.bootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
